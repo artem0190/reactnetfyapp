1cae2632741d1fea0072d3dd99d617b88d5838b0
unknown <polimon0190123456789@gmail.com>
2023-01-18 21:58:32 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import {Header, Main, Footer } from './containers';
+import './App.css'
+
+export const App = () => {
+  return (
+    <div className='App'>
+        <Header/>
+        <Main/>
+        <Footer/>
+    </div>
+
+  )
+}
+
+export default App;
---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './main.css'
+
+const Main = () => {
+  return (
+    <div className="container"></div>
+  )
+}
+
+export default Main
